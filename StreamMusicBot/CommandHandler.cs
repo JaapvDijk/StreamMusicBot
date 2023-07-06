09dab9716e8bb48e28c6a71d68074878639d73f0
Jaap van Dijk <dijkvanjaap@hotmail.com>
2023-07-06 13:12:21 +0200

    new docker reg

--- StreamMusicBot/CommandHandler.cs
+++ StreamMusicBot/CommandHandler.cs
@@ -1,14 +1,14 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using Microsoft.Extensions.Logging;
 using Serilog;
-using StreamMusicBot.Services;
 using System;
-using System.Net.NetworkInformation;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Victoria;
+using static SpotifyAPI.Web.PlaylistRemoveItemsRequest;
 
 namespace StreamMusicBot
 {
@@ -50,30 +50,28 @@ namespace StreamMusicBot
 
         private async Task HandleMessageAsync(SocketMessage socketMessage)
         {
+            var argPos = 0;
+            var CommandPrefixChar = "!";
+
+            if (socketMessage.Author.IsBot) return;
+
+            var userMessage = socketMessage as SocketUserMessage;
+            if (userMessage is null)
+                return;
+
+            //TODO "!" prefix (commented out stuff no worki)
+            if (!userMessage.HasMentionPrefix(_client.CurrentUser, ref argPos) &&
+                !userMessage.HasStringPrefix(CommandPrefixChar, ref argPos, StringComparison.OrdinalIgnoreCase))
+                return;
+
+            var context = new SocketCommandContext(_client, userMessage);
             try
             {
-                var argPos = 0;
-                var CommandPrefixChar = "!";
-
-                if (socketMessage.Author.IsBot) return;
-
-                var userMessage = socketMessage as SocketUserMessage;
-                if (userMessage is null)
-                    return;
-
-                //TODO "!" prefix (commented out stuff no worki)
-                if (!userMessage.HasMentionPrefix(_client.CurrentUser, ref argPos) &&
-                    !userMessage.HasStringPrefix(CommandPrefixChar, ref argPos, StringComparison.OrdinalIgnoreCase))
-                    return;
-
-                Log.Information($"Received command message {userMessage.Content} -"); //msg empty??
-
-                var context = new SocketCommandContext(_client, userMessage);
                 await _cmdService.ExecuteAsync(context, argPos, _services);
             }
             catch(Exception ex) 
             { 
-                Log.Information($"MessageReceived handler ex: {ex.Message}");
+                Log.Information($"Could not run command, exception: {ex.Message}");
             }
         }
 
